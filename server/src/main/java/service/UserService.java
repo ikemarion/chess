1ba07ddda63587f7d680ef00ee6f4672e9f5f429
ikemarion <ikemarion@gmail.com>
2025-03-03 17:39:30 -0700

    Added more classes

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,48 @@
+package service;
+
+import dataaccess.DataAccessException;
+
+import java.util.UUID;
+
+public class UserService {
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO){
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public AuthData register(String username, String password, String email) throws DataAccessException{
+        if(username == null || password == null || email == null || username.isEmpty() || password.isEmpty() || email.isEmpty()){
+            throw new DataAccessException("Error: bad request");
+        }
+        UserData newUser = new UserData(username, password, email);
+        userDAO.createUser(newUser);
+
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, username);
+        authDAO.createAuth(authData);
+
+        return authData;
+    }
+
+    public AuthData login(String username, String password) throws DataAccessException{
+        if(username == null || password == null || username.isEmpty() || password.isEmpty()){
+            throw new DataAccessException("Error: bad request");
+        }
+        if(!userDAO.authenticateUser(username, password)){
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, username);
+        authDAO.createAuth(authData);
+
+        return  authData;
+    }
+
+    public void logout(String authToken) throws DataAccessException{
+        authDAO.deleteAuth(authToken);
+    }
+}
